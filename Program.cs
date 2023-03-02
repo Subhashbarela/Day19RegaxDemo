3b72679da40b3ae648252c0bc7f7e889b4321437
Subhash Barela <barelasubhash16@gmail.com>
2023-03-02 12:08:30 +0530

    create program tha first char should be cap

--- Program.cs
+++ Program.cs
@@ -10,7 +10,20 @@ namespace RegaxDemoProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to regax program :");
+            Patterns pattern = new Patterns();
+
+            Console.WriteLine("Enter your First Name: ");
+            string name = Console.ReadLine();
+            bool val = pattern.validate(name);
+
+            if (val)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name!");
+            }
             Console.Read();
         }
     }
