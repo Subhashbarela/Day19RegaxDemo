7b6341cbdcceb19256d4d854b1fb42e5a06b25f1
Subhash Barela <barelasubhash16@gmail.com>
2023-03-02 12:25:05 +0530

    last name should be cap

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegaxDemoConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns pattern = new Patterns();
+
+            Console.WriteLine("Enter your Last Name: ");
+            string name = Console.ReadLine();
+            bool val = pattern.validate(name);
+
+            if (val)
+            {
+                Console.WriteLine("Valid Last Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Last Name!");
+            }
+        }
+    }
+}
